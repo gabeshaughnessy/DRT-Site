837cc9678fa256fbd23ac98699e02ac4b617e624
gabeshaughnessy <gabeshaughnessy@gmail.com>
2012-07-18 14:55:22 -0700

    fixed another error

--- themes/distilleryrowtours/archive-drt_spirit.php
+++ themes/distilleryrowtours/archive-drt_spirit.php
@@ -1,6 +1,6 @@
 <?php 
 get_header();
-get_template_part('archive_intro', 'spirit')
+get_template_part('archive_intro', 'spirit');
 get_template_part('content_block', 'partner_archive');
 get_footer();
 ?>
